2522ec048f280da7dfd298b9a5ffd325d2de3be6
Inkesiter <truongduchainguyen@outlook.com>
2021-10-11 21:52:35 +0700

    da xong phan tim component de crawl

--- crawl.py
+++ crawl.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 def crawl(country, tab):
-    # driver = webdriver.Chrome("chromedriver_win32/chromedriver") ###### ubuntu nhà tôi chạy Firefox nên các gs pull về đổi chrome cũng được nhé
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome("chromedriver_win32/chromedriver") ###### ubuntu nhà tôi chạy Firefox nên các gs pull về đổi chrome cũng được nhé
+    # driver = webdriver.Firefox()
     URL = "https://www.chiasenhac.vn/mp3/" + country + ".html?tab=" + tab
     arr = np.arange(1, 20)
 
@@ -16,11 +16,12 @@ def crawl(country, tab):
     for i in arr:
         driver.get(URL + "&page=" + str(i))
         main = driver.find_element_by_class_name('content-current')
-        a = main.find_elements_by_tag_name("a")
-        for j in a:
-            link = j.get_attribute("href")
+        songs = main.find_elements_by_css_selector('.media.align-items-stretch.not')
+        for song in songs:
+            a = song.find_element_by_tag_name("a")
+            link = a.get_attribute("href")
+            print(link)
             link_list.append(link)
-    print(link_list)
 
 if __name__ == "__main__":
     country = input()
