09f506c13cbd9b4f8e372bff7587d43a41c8e1c3
truongduchainguyen <truongduchainguyen@gmail.com>
2021-10-11 07:41:24 +0700

    tiep tuc crawl.py

--- crawl.py
+++ crawl.py
@@ -3,11 +3,21 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 def crawl(country, tab):
-    driver = webdriver.Chrome("chromedriver_win32/chromedriver")
-    URL = "https://www.chiasenhac.vn/mp3/" + country + "?tab=" + tab + "&page=1"
-    driver.get(URL)
+    # driver = webdriver.Chrome("chromedriver_win32/chromedriver") ###### ubuntu nhà tôi chạy Firefox nên các gs pull về đổi chrome cũng được nhé
+    driver = webdriver.Firefox()
+    URL = "https://www.chiasenhac.vn/mp3/" + country + ".html?tab=" + tab
+    arr = np.arange(1, 20)
+
+    save_path = 'data/'
+
+
+    for i in arr:
+        driver.get(URL + "&page=" + str(i))
+        main = driver.find_elements_by_class_name('bai-hat-moi')
+        print(main)
 
 if __name__ == "__main__":
     country = input()
     tab = input()
     crawl(country, tab)
+
