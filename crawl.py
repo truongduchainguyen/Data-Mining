a2e77c8d6eb36110000e763c1ddaf747bfd308ca
truongduchainguyen <truongduchainguyen@gmail.com>
2021-10-11 17:24:18 +0700

    tiep tuc crawl.py

--- crawl.py
+++ crawl.py
@@ -10,11 +10,17 @@ def crawl(country, tab):
 
     save_path = 'data/'
 
+    # link_list = np.empty(1, dtype=str)
+    link_list = []
 
     for i in arr:
         driver.get(URL + "&page=" + str(i))
-        main = driver.find_elements_by_class_name('bai-hat-moi')
-        print(main)
+        main = driver.find_element_by_class_name('content-current')
+        a = main.find_elements_by_tag_name("a")
+        for j in a:
+            link = j.get_attribute("href")
+            link_list.append(link)
+    print(link_list)
 
 if __name__ == "__main__":
     country = input()
