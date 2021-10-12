1ead935300e7011b6c4d80af02e01a7c457f308c
truongduchainguyen <truongduchainguyen@gmail.com>
2021-10-12 10:53:30 +0700

    tiep tuc crawl.py

--- crawl.py
+++ crawl.py
@@ -1,17 +1,26 @@
 import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import urllib3
+import os
+
+def create_link_file(list_link):
+    textfile = open("list_song.txt", "w")
+    for element in list_link:
+        textfile.write(element + "\n")
+    textfile.close()
+
+# def open_link_file(filename):
+
 
 def crawl(country, tab):
-    driver = webdriver.Chrome("chromedriver_win32/chromedriver") ###### ubuntu nhà tôi chạy Firefox nên các gs pull về đổi chrome cũng được nhé
-    # driver = webdriver.Firefox()
+    # driver = webdriver.Chrome("chromedriver_win32/chromedriver") ###### ubuntu nhà tôi chạy Firefox nên các gs pull về đổi chrome cũng được nhé
+    driver = webdriver.Firefox()
     URL = "https://www.chiasenhac.vn/mp3/" + country + ".html?tab=" + tab
     arr = np.arange(1, 20)
-
     save_path = 'data/'
-
-    # link_list = np.empty(1, dtype=str)
     link_list = []
+    req = urllib3.PoolManager()
 
     for i in arr:
         driver.get(URL + "&page=" + str(i))
@@ -20,8 +29,21 @@ def crawl(country, tab):
         for song in songs:
             a = song.find_element_by_tag_name("a")
             link = a.get_attribute("href")
-            print(link)
+            # print(link)
             link_list.append(link)
+
+    driver.close()
+
+    for i in link_list:
+        driver.get(i)
+        prepare = driver.find_element_by_id("pills-download-tab").click()
+        download = prepare.find_element_by_class_name("download_item")
+        print(download.text)
+
+        # res = req.request('GET', i)
+        # soup = bs(res.data, 'html.parser')
+
+
 
 if __name__ == "__main__":
     country = input()
